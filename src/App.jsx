a85d42bf1ab41b3aaf377df03616f867a9670171
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-06-24 23:49:53 -0500

    date

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,37 @@ function App() {
     .catch(err => console.log("axios err: ", err))
 }, [])
 
+function formatDate(date) {
+  var d = new Date(date);
+  var hh = d.getHours();
+  var m = d.getMinutes();
+  var s = d.getSeconds();
+  var dd = "AM";
+  var h = hh;
+  if (h >= 12) {
+    h = hh - 12;
+    dd = "PM";
+  }
+  if (h == 0) {
+    h = 12;
+  }
+  m = m < 10 ? "0" + m : m;
+
+  s = s < 10 ? "0" + s : s;
+
+  /* if you want 2 digit hours:
+  h = h<10?"0"+h:h; */
+
+  var pattern = new RegExp("0?" + hh + ":" + m + ":" + s);
+
+  var replacement = h + ":" + m;
+  /* if you want to add seconds
+  replacement += ":"+s;  */
+  replacement += " " + dd;
+
+  return date.replace(pattern, replacement);
+}
+
 
 
   return (
@@ -34,7 +65,7 @@ function App() {
         console.log(s)
         return (
           <div className='wrapper'>
-            <h1>{s}</h1>
+            <h1>{formatDate(s)}</h1>
           </div>
         )
       })}
